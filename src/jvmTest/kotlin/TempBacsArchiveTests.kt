78f6bf36e466fb18d7d8b6505a05a8316c3dc15b
Rustam Musin <jvmusin@gmail.com>
2021-02-01 01:21:22 +0400

    Update Bacs Archive tests to migrate from retrofit in future

--- src/jvmTest/kotlin/TempBacsArchiveTests.kt
+++ src/jvmTest/kotlin/TempBacsArchiveTests.kt
@@ -1,18 +1,19 @@
-import bacs.BacsArchiveApi
-import bacs.uploadProblem
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import io.kotest.core.annotation.Ignored
 import io.kotest.core.spec.style.StringSpec
-import kotlinx.serialization.json.Json
+import kotlinx.serialization.ExperimentalSerializationApi
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
+import okhttp3.logging.HttpLoggingInterceptor.Level.HEADERS
+import org.jsoup.Jsoup
 import sybon.SybonApiFactory
 import java.nio.file.Paths
 
+@OptIn(ExperimentalSerializationApi::class)
 @Ignored
 class TempBacsArchiveTests : StringSpec({
     val api = SybonApiFactory().createArchiveApi()
@@ -25,21 +26,45 @@ class TempBacsArchiveTests : StringSpec({
     }
 
     "send problem to bacs archive" {
-        val client = OkHttpClient().newBuilder().addInterceptor(HttpLoggingInterceptor {
-            println(it)
-        }.setLevel(HttpLoggingInterceptor.Level.HEADERS))
-            .addInterceptor {
-                it.proceed(it.request().newBuilder().header("Authorization", "Basic c3lib246d2poJDQyZHMwOQ==").build())
-            }
+        val zip = Paths.get("4-values-sum-0-low-tl-package174473.zip")
+        val body = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("_5", "Upload")
+            .addFormDataPart(
+                "archive",
+                zip.fileName.toString(),
+                zip.toFile().asRequestBody("application/zip".toMediaType())
+            )
+            .addFormDataPart("archiver_format", "")
+            .addFormDataPart("archiver_type", "7z")
+            .addFormDataPart("response", "html")
             .build()
-        val api = Retrofit.Builder()
-            .baseUrl("https://archive.bacs.cs.istu.ru/repository/")
-            .client(client)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(Json { isLenient = true }.asConverterFactory("application/json".toMediaType()))
+
+        val loggingInterceptor = HttpLoggingInterceptor(::println).setLevel(HEADERS)
+        val authInterceptor = Interceptor() {
+            val request = it.request().newBuilder()
+                .header("Authorization", "Basic c3lib246d2poJDQyZHMwOQ==")
+                .build()
+            it.proceed(request)
+        }
+        val client = OkHttpClient().newBuilder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
             .build()
-            .create<BacsArchiveApi>()
-        val path = Paths.get("4-values-sum-0-low-tl-package174473.zip")
-        println(api.uploadProblem(path))
+        val request = Request.Builder()
+            .url("https://archive.bacs.cs.istu.ru/repository/upload")
+            .post(body)
+            .build()
+        val response = client.newCall(request).execute()
+
+        val document = Jsoup.parse(response.body!!.string())
+        val element = document.body()
+            .getElementsByTag("table")[0]
+            .getElementsByTag("tbody")[0]
+            .getElementsByTag("tr")[1]
+            .getElementsByTag("td")[2]
+            .getElementsByTag("pre")[0]
+            .text()
+        println(element)
     }
 })
