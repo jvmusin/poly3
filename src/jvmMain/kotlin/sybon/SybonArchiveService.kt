5314c7fd587b0cb99eec6f2ab8f43a9783ac7d75
Rustam Musin <jvmusin@gmail.com>
2021-02-17 17:32:29 +0400

    Merge interfaces and implementation files

--- src/jvmMain/kotlin/sybon/SybonArchiveService.kt
+++ src/jvmMain/kotlin/sybon/SybonArchiveService.kt
@@ -2,11 +2,34 @@
 
 package sybon
 
+import sybon.api.SybonArchiveApi
 import sybon.api.SybonProblem
 import util.RetryPolicy
+import util.getLogger
 import kotlin.time.ExperimentalTime
 
 interface SybonArchiveService {
     suspend fun getProblems(): List<SybonProblem>
     suspend fun importProblem(bacsProblemId: String, retryPolicy: RetryPolicy = RetryPolicy()): SybonProblem?
+}
+
+class SybonArchiveServiceImpl(
+    private val sybonArchiveApi: SybonArchiveApi,
+    private val collectionId: Int
+) : SybonArchiveService {
+
+    override suspend fun getProblems() = sybonArchiveApi.getCollection(collectionId).problems
+
+    override suspend fun importProblem(bacsProblemId: String, retryPolicy: RetryPolicy): SybonProblem? {
+        getLogger(javaClass).debug("Checking if the problem is already in this collection")
+        getProblems().singleOrNull { it.internalProblemId == bacsProblemId }?.let {
+            getLogger(javaClass).debug("The problem is already in this collection")
+            return it
+        }
+        getLogger(javaClass).debug("The problem is not in this collection, importing")
+        sybonArchiveApi.importProblem(collectionId, bacsProblemId)
+        return retryPolicy.eval {
+            getProblems().singleOrNull { it.internalProblemId == bacsProblemId }
+        }
+    }
 }
--- src/jvmMain/kotlin/sybon/SybonCheckingService.kt
+++ src/jvmMain/kotlin/sybon/SybonCheckingService.kt
@@ -1,8 +1,11 @@
 package sybon
 
+import sybon.api.SybonCheckingApi
 import sybon.api.SybonCompiler
 import sybon.api.SybonSubmissionResult
+import sybon.api.SybonSubmitSolution
 import util.RetryPolicy
+import util.encodeBase64
 
 interface SybonCheckingService {
     suspend fun getResult(id: Int): SybonSubmissionResult
@@ -12,4 +15,46 @@ interface SybonCheckingService {
         compiler: SybonCompiler,
         retryPolicy: RetryPolicy = RetryPolicy()
     ): SybonFinalSubmissionResult
+}
+
+class SybonCheckingServiceImpl(
+    private val sybonCheckingApi: SybonCheckingApi
+) : SybonCheckingService {
+
+    override suspend fun getResult(id: Int) = sybonCheckingApi.getResults(id.toString()).single()
+
+    override suspend fun submitSolution(
+        problemId: Int,
+        solution: String,
+        compiler: SybonCompiler,
+        retryPolicy: RetryPolicy
+    ): SybonFinalSubmissionResult {
+        val submissionId = sybonCheckingApi.submitSolution(
+            SybonSubmitSolution(
+                compilerId = compiler.id,
+                problemId = problemId,
+                solution = solution.encodeBase64(),
+                continueCondition = SybonSubmitSolution.ContinueCondition.WhileOk
+            )
+        )
+
+        val result = retryPolicy.eval {
+            val result = getResult(submissionId)
+            if (result.buildResult.status != SybonSubmissionResult.BuildResult.Status.PENDING) result
+            else null
+        } ?: throw SybonSubmitSolutionException("Решение не было отправлено")
+
+        val failedGroupIndex = result.testGroupResults.indexOfFirst { group ->
+            !group.executed || group.testResults.any { testResult ->
+                testResult.status != SybonSubmissionResult.TestGroupResult.TestResult.Status.OK
+            }
+        }
+        if (failedGroupIndex == -1) return SybonFinalSubmissionResult(true, null, null)
+        val indexInGroup = result.testGroupResults[failedGroupIndex].testResults.indexOfFirst {
+            it.status != SybonSubmissionResult.TestGroupResult.TestResult.Status.OK
+        }
+        val testIndex = result.testGroupResults.take(failedGroupIndex).sumOf { it.testResults.size } + indexInGroup
+        val testStatus = result.testGroupResults[failedGroupIndex].testResults[indexInGroup].status
+        return SybonFinalSubmissionResult(true, testIndex, testStatus)
+    }
 }
