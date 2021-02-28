ea3b6df02efb99ff3f12f53dab859375d7b03ecc
Rustam Musin <jvmusin@gmail.com>
2021-02-28 13:34:27 +0400

    Add more tests for the PolygonApi.getTestGroup method

--- src/jvmTest/kotlin/PolygonApiTests.kt
+++ src/jvmTest/kotlin/PolygonApiTests.kt
@@ -12,7 +12,6 @@ import polygon.TestGroup.PointsPolicyType.COMPLETE_GROUP
 import polygon.TestGroup.PointsPolicyType.EACH_TEST
 
 class PolygonApiTests : BehaviorSpec(), KoinTest {
-    private val problemId = 159528
     private val api: PolygonApi by inject()
 
     override fun listeners() = listOf(KoinListener(polygonModule))
@@ -21,6 +20,7 @@ class PolygonApiTests : BehaviorSpec(), KoinTest {
         Given("getTestGroup") {
             When("asking for problem with test group on all tests") {
 
+                val problemId = 159528
                 suspend fun result() = api.getTestGroup(problemId, null).result!!
                 val expectedGroups = listOf("samples", "first", "second", "third", "fourth", "fifth")
 
@@ -29,6 +29,69 @@ class PolygonApiTests : BehaviorSpec(), KoinTest {
                 }
 
                 Then("returns correct group dependencies") {
+                    val byName = result().associateBy { it.name }
+                    byName["second"]!!.dependencies should containExactlyInAnyOrder("samples")
+                    byName["third"]!!.dependencies should containExactlyInAnyOrder("first", "fifth")
+                    byName["fourth"]!!.dependencies should containExactlyInAnyOrder("third")
+                    (expectedGroups - setOf("second", "third", "fourth")).forAll {
+                        byName[it]!!.dependencies.shouldBeEmpty()
+                    }
+                }
+
+                Then("all groups except fourth should have points policy of EACH_TEST") {
+                    val result = result().associateBy { it.name }
+                    result.entries.filter { it.key != "fourth" }.map { it.value }
+                        .forAll { it.pointsPolicy shouldBe EACH_TEST }
+                    result["fourth"]!!.pointsPolicy shouldBe COMPLETE_GROUP
+                }
+
+                Then("fourth group should have points policy of COMPLETE_GROUP") {
+                    result().single { it.name == "fourth" }.pointsPolicy shouldBe COMPLETE_GROUP
+                }
+            }
+
+            When("asking for problem with test group on all tests except samples") {
+
+                val problemId = 159558
+                suspend fun result() = api.getTestGroup(problemId, null).result!!
+                val expectedGroups = listOf("first", "second", "third", "fourth", "fifth")
+
+                Then("returns correct group names") {
+                    result().map { it.name } should containExactlyInAnyOrder(expectedGroups)
+                }
+
+                Then("returns correct group dependencies") {
+                    val byName = result().associateBy { it.name }
+                    byName["third"]!!.dependencies should containExactlyInAnyOrder("first", "fifth")
+                    byName["fourth"]!!.dependencies should containExactlyInAnyOrder("third")
+                    (expectedGroups - setOf("third", "fourth")).forAll {
+                        byName[it]!!.dependencies.shouldBeEmpty()
+                    }
+                }
+
+                Then("all groups except fourth should have points policy of EACH_TEST") {
+                    val result = result().associateBy { it.name }
+                    result.entries.filter { it.key != "fourth" }.map { it.value }
+                        .forAll { it.pointsPolicy shouldBe EACH_TEST }
+                    result["fourth"]!!.pointsPolicy shouldBe COMPLETE_GROUP
+                }
+
+                Then("fourth group should have points policy of COMPLETE_GROUP") {
+                    result().single { it.name == "fourth" }.pointsPolicy shouldBe COMPLETE_GROUP
+                }
+            }
+
+            When("asking for problem with test groups and no points") {
+
+                val problemId = 159559
+                suspend fun result() = api.getTestGroup(problemId, null).result!!
+                val expectedGroups = listOf("samples", "first", "second", "third", "fourth", "fifth")
+
+                Then("returns correct group names") {
+                    result().map { it.name } should containExactlyInAnyOrder(expectedGroups)
+                }
+
+                Then("returns group dependencies as if they were shown on the page") {
                     val byName = result().associateBy { it.name }
                     byName["second"]!!.dependencies should containExactlyInAnyOrder("samples")
                     byName["third"]!!.dependencies should containExactlyInAnyOrder("first", "fifth")
