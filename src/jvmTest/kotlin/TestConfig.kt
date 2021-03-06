8fdfb9deddeda1faabf317b3ad339450bb658f5c
Rustam Musin <jvmusin@gmail.com>
2021-03-06 19:56:27 +0400

    Simplify test configs

--- src/jvmTest/kotlin/TestConfig.kt
+++ src/jvmTest/kotlin/TestConfig.kt
@@ -1,18 +1,11 @@
-import bacs.BacsConfig
 import bacs.bacsModule
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import io.github.config4k.extract
-import polygon.PolygonConfig
 import polygon.polygonModule
-import sybon.SybonConfig
 import sybon.sybonModule
 
 val config: Config = ConfigFactory.load()
-val polygonConfig: PolygonConfig = config.extract("polygon")
-val bacsConfig: BacsConfig = config.extract("bacs")
-val sybonConfig: SybonConfig = config.extract("sybon")
-
-val bacsModule = bacsModule(bacsConfig)
-val polygonModule = polygonModule(polygonConfig)
-val sybonModule = sybonModule(sybonConfig)
+val polygonModule get() = polygonModule(config.extract("polygon"))
+val bacsModule get() = bacsModule(config.extract("bacs"))
+val sybonModule get() = sybonModule(config.extract("sybon"))
