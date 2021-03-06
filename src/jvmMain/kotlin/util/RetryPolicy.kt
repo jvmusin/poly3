f72ebfb194ec384982630798836f198773783afa
Rustam Musin <jvmusin@gmail.com>
2021-03-06 22:22:28 +0400

    Add docs for RetryPolicy

--- src/jvmMain/kotlin/util/RetryPolicy.kt
+++ src/jvmMain/kotlin/util/RetryPolicy.kt
@@ -9,25 +9,62 @@ import kotlin.time.TimeSource
 import kotlin.time.minutes
 import kotlin.time.seconds
 
+/**
+ * Retry policy.
+ *
+ * Allows to retry some action for the given period of time with some delay until some condition is met.
+ *
+ * @property tryFor Period of time to try to evaluate the function.
+ * @property retryAfter Period of time to wait until the next try after fail.
+ */
 data class RetryPolicy(
     val tryFor: Duration = 5.minutes,
     val retryAfter: Duration = 5.seconds
 ) {
 
+    /**
+     * Evaluates the given [function] at least once.
+     * Returns result as soon as [check] returns *true* for the first time
+     * or when [tryFor] period is gone.
+     *
+     * Does retries every [retryAfter] until [tryFor] time is gone.
+     *
+     * The returned value is either the result of [function] when [check] returned *true* for the first time or
+     * the last calculated by [function] value if [tryFor] time is gone.
+     *
+     * @param T type of the result.
+     * @param check function to check if the result is correct and can be returned.
+     * @param function function to generate result.
+     * @return First successful or last calculated result.
+     */
     @Suppress("REDUNDANT_INLINE_SUSPEND_FUNCTION_TYPE")
-    suspend inline fun <T> evalWhileFails(check: (T?) -> Boolean, func: suspend () -> T?): T? {
+    suspend inline fun <T> evalWhileFails(check: (T?) -> Boolean, function: suspend () -> T?): T? {
         val start = TimeSource.Monotonic.markNow()
         var first = true
         var res: T? = null
         while (first || start.elapsedNow() < tryFor) {
             first = false
-            res = func()
+            res = function()
             if (check(res)) return res
             delay(retryAfter)
         }
         return res
     }
 
+    /**
+     * Evaluates the given [function] at least once.
+     * Returns result as soon as the result is not *null* for the first time
+     * or when [tryFor] period is gone.
+     *
+     * Does retries every [retryAfter] until [tryFor] time is gone.
+     *
+     * The returned value is either the result of [function] when it's not *null* for the first time or
+     * the last calculated by [function] value if [tryFor] time is gone.
+     *
+     * @param T type of the result.
+     * @param function function to generate result.
+     * @return First not null or last calculated result.
+     */
     @Suppress("REDUNDANT_INLINE_SUSPEND_FUNCTION_TYPE")
-    suspend inline fun <T> evalWhileNull(func: suspend () -> T?) = evalWhileFails({ it != null }, func)
+    suspend inline fun <T> evalWhileNull(function: suspend () -> T?) = evalWhileFails({ it != null }, function)
 }
