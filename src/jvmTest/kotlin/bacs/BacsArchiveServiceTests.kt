5706dd3d9470a87caee72b035e78763702426f66
Rustam Musin <jvmusin@gmail.com>
2021-03-08 21:46:10 +0400

    Repeat bacs tests 3 times since they are sadly unstable

--- src/jvmTest/kotlin/bacs/BacsArchiveServiceTests.kt
+++ src/jvmTest/kotlin/bacs/BacsArchiveServiceTests.kt
@@ -3,12 +3,17 @@ package bacs
 import bacs.BacsProblemState.IMPORTED
 import bacs.BacsProblemState.NOT_FOUND
 import bacsModule
+import io.kotest.assertions.retry
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.koin.KoinListener
 import io.kotest.matchers.shouldBe
 import org.koin.test.KoinTest
 import org.koin.test.inject
+import kotlin.time.ExperimentalTime
+import kotlin.time.minutes
+import kotlin.time.seconds
 
+@OptIn(ExperimentalTime::class)
 class BacsArchiveServiceTests : BehaviorSpec(), KoinTest {
     override fun listeners() = listOf(KoinListener(bacsModule))
     private val service: BacsArchiveService by inject()
@@ -17,12 +22,16 @@ class BacsArchiveServiceTests : BehaviorSpec(), KoinTest {
         Given("getProblemState") {
             When("problem is imported correctly") {
                 Then("returns status IMPORTED") {
-                    service.getProblemState("polybacs-frog-and-polygon-ok") shouldBe IMPORTED
+                    retry(3, 5.minutes, 3.seconds) {
+                        service.getProblemState("polybacs-frog-and-polygon-ok") shouldBe IMPORTED
+                    }
                 }
             }
             When("problem does not exist") {
                 Then("returns status NOT_FOUND") {
-                    service.getProblemState("not-existing-problem") shouldBe NOT_FOUND
+                    retry(3, 5.minutes, 3.seconds) {
+                        service.getProblemState("not-existing-problem") shouldBe NOT_FOUND
+                    }
                 }
             }
         }
