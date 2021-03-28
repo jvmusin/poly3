6125563249faeecc9ab6b64e0b1a55c9e9c0c82e
Rustam Musin <jvmusin@gmail.com>
2021-03-28 19:01:40 +0400

    Drop PolygonService

--- src/jvmMain/kotlin/util/ZipUtils.kt
+++ src/jvmMain/kotlin/util/ZipUtils.kt
@@ -8,23 +8,28 @@ import java.nio.file.Path
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
 import java.util.zip.ZipOutputStream
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.createDirectories
 import kotlin.streams.toList
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
+/** Extracts content of `this` [ZipFile] to [destination] folder. */
+@OptIn(ExperimentalPathApi::class)
 fun ZipFile.extract(destination: Path) {
     Files.createDirectories(destination)
     for (entry in entries()) {
         if (entry.isDirectory) {
-            Files.createDirectories(destination.resolve(entry.name))
+            destination.resolve(entry.name).createDirectories()
         } else {
             getInputStream(entry).use {
-                Files.write(destination.resolve(entry.name), it.readAllBytes())
+                destination.resolve(entry.name).toFile().writeBytes(it.readAllBytes())
             }
         }
     }
 }
 
+/** Packs content of `this` [Path] to [destination] file. Rewrites [destination] file if it exists. */
 @OptIn(ExperimentalTime::class)
 fun Path.toZipArchive(destination: Path) {
     val (zipPath, duration) = measureTimedValue {
